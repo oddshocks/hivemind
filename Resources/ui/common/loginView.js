2323b52e1ad455571705b21a13ff1be5ccfd8281
dweebles <dqn8613@rit.edu>
2013-03-27 23:21:31 -0400

    Login view update

--- Resources/ui/common/loginView.js
+++ Resources/ui/common/loginView.js
@@ -1,23 +1,34 @@
-function DetailView() {
-	var self = Ti.UI.createView();
-	
-	var lbl = Ti.UI.createLabel({
-		text:'login',
-		height:'auto',
-		width:'auto',
-		color:'#000'
-	});
-	self.add(lbl);
-	
-	self.addEventListener('login', function(e) {
-		
-	});
-	
-	self.addEventListener('signup', function(e) {
-		
-	});
-	
-	return self;
+function loginView (){
+	var win = Ti.UI.createWindow({
+  fullscreen: true
+});
+var LOGIN = 1,  signUp = 2;
+var loggedIn = false;
+
+var activity = win.activity;
+
+activity.onCreateOptionsMenu = function(e){
+  var menu = e.menu;
+  var login = menu.add({ title: "Login", itemId: LOGIN });
+  login.setIcon("login.png");
+  login.addEventListener("click", function(e) {
+    loggedIn = true;
+    // In Android 3.0 and above we need to call invalidateOptionsMenu() for menu changes at runtime
+    win.activity.invalidateOptionsMenu();
+  });
+  var signup = menu.add({ title: "Sign up", itemId: signUp });
+  logout.setIcon("logout.png");
+  logout.addEventListener("click", function(e) {
+    loggedIn = false;
+    // In Android 3.0 and above we need to call invalidateOptionsMenu() for menu changes at runtime
+    win.activity.invalidateOptionsMenu();
+  });
 };
 
-module.exports = loginView;
+activity.onPrepareOptionsMenu = function(e) {
+  var menu = e.menu;
+  menu.findItem(LOGIN).setVisible(!loggedIn);
+  menu.findItem(signUp).setVisible(loggedIn);
+};
+win.open();
+}
