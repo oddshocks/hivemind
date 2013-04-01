65dc644267b6ea2045d47ffc9c70139e7dc4db89
dweebles <dqn8613@rit.edu>
2013-04-01 11:24:26 -0400

    app.js URL redirect

--- Resources/app.js
+++ Resources/app.js
@@ -39,6 +39,6 @@ if (Ti.version < 1.8 ) {
 Titanium.UI.setBackgroundColor('#fff');    
 var login = Titanium.UI.createWindow({  
      
-    url:'common/loginView.js'  
+    url:'loginView.js'  
 });
 login.open();
--- Resources/ui/common/loginView.js
+++ Resources/ui/common/loginView.js
@@ -1,22 +1,3 @@
-// function loginView (){
-// 	var win = Ti.UI.currentWindow({
-//   fullscreen: true
-// });
-// // this sets the background color of the master UIView (when there are no windows/tab groups on it)  
-	// Titanium.UI.setBackgroundColor('#fff');  
-	// 	var tabGroup = Titanium.UI.createTabGroup();  
-	// 	var login = Titanium.UI.createWindow({  
- //    		title:'Enter the Hive',  
- //    		tabBarHidden:true,  
- //    		url:'main_windows/login.js'  
-	// });  
-	// var loginTab = Titanium.UI.createTab({  
- //    		title:"Login",  
- //    		window:login  
-	// });  
-	// tabGroup.addTab(loginTab);  
-	// tabGroup.open();
-
 var win = Titanium.UI.currentWindow;
 
 	var username = Titanium.UI.createTextField({
