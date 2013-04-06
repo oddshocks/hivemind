3fc99154e285725ebf00502afd25c53434e91a9c
dweebles <dqn8613@rit.edu>
2013-04-06 15:14:21 -0400

    fix syntax in app.js

--- Resources/ui/handheld/android/ApplicationWindow.js
+++ Resources/ui/handheld/android/ApplicationWindow.js
@@ -1,13 +1,23 @@
-// function ApplicationWindow() {
+function ApplicationWindow() {
+	var Cloud = ('ti.cloud');
+
 	// //declare module dependencies
-	// var MasterView = require('ui/common/MasterView'),
-		// DetailView = require('ui/common/DetailView');
-		// loginView = require('ui/common/loginView');
-	// // var Cloud = ('ti.cloud')	;
-	// // var idBool = new Boolean();
-	// // var idExists = get.sessionId();
-	// //var db = openDatabase('db.sqlite', '3.0', 'HiveMind', 20000);
-	// //var userAuth;
+	 var MasterView = require('ui/common/MasterView'),
+	 DetailView = require('ui/common/DetailView');
+	 loginView = require('ui/common/loginView');
+
+	 var Gravitar = require('ui/common/gravitas');
+
+	 var win = ti.UI.CreateWindow();
+	 var userPic = Gravitas.createGravitar({
+	 	email: "dqn8613@rit.edu",
+	 	defaultStyle: Gravitas_DEFAULT_CUSTOM,
+	 	size: 250,
+	 	rating: Gravitas_RATING_R;
+	 });
+	 
+	//var db = openDatabase('db.sqlite', '3.0', 'HiveMind', 20000);
+	//var userAuth;
 // 
 // 		
 	// //create object instance
@@ -32,3 +42,12 @@
 // };
 // 
 // module.exports = ApplicationWindow;
+headerView = Ti.UI.createView(S.combine($$.headerView,{top:0})),
+		tabHeight = 60,
+		tabWidth = platformWidth/3,
+		tabView = Ti.UI.createView({
+			bottom:0,
+			height:tabHeight,
+			backgroundColor: "black",
+			width:platformWidth
+		}),
