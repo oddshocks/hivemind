6da863206648bfe814bfd8b64cb15331a868e3d6
dweebles <dqn8613@rit.edu>
2013-04-19 12:49:31 -0400

    latest

--- Resources/ui/common/feedView.js
+++ Resources/ui/common/feedView.js
@@ -1,7 +1,6 @@
 var win = Ti.UI.currentWindow;
 
 var header = Ti.UI.createView({
-	backgroundColor: '#FFF',
 	width: '100%',
 	height: '33%',
 	top: 0
@@ -17,6 +16,40 @@ var header = Ti.UI.createView({
 	});
 header.add(chooseHive);
 	
+var navigation = Ti.UI.createView({
+	height: '40%',
+	width: '100%',
+	backgroundColor: '#FFF'
+});
+	var hive1 = Ti.UI.createButton({
+		title: 'Hive 1',
+		height: 75,
+		width: 75,
+		left: 10
+	});
+navigation.add(hive1);
 
+	var hive2= Ti.UI.createButton({
+		title: 'Hive 2',
+		height: 75,
+		width: 75,
+		left: 95
+	});
+navigation.add(hive2);
+	var hive3 = Ti.UI.createButton({
+		title: 'Hive 3',
+		height: 75,
+		width: 75,
+		left: 180
+	});
+navigation.add(hive3);
+	var hive4 = Ti.UI.createButton({
+		title: 'Hive 4',
+		height: 75,
+		width: 75,
+		left: 265
+	});
+navigation.add(hive4);
 
+header.add(navigation);
 win.add(header);
